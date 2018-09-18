b82dfea7de0d87773a654d5d915f7eab0258de9b
shortnd <ococncol@gmail.com>
2018-09-17 22:41:30 -0400

    added routes to chat controller

--- routes/chat.js
+++ routes/chat.js
@@ -1,10 +1,39 @@
 const express = require('express');
 
 const router = express.Router();
+const mongoose = require('mongoose');
+const Chat = require('../models/Chat');
 
-// Get home page
+// Get ALL CHATS
 router.get('/', (req, res, next) => {
-  res.send('Express RESTful API');
+  Chat.find((err, chats) => {
+    if (err) return next(err);
+    res.json(chats);
+  });
+});
+
+// GET SINGLE CHAT BY ID
+router.get('/:id', (req, res, next) => {
+  Chat.findById(req.params.id, (err, chat) => {
+    if (err) return next(err);
+    res.json(chat);
+  });
+});
+
+// SAVE CHAT
+router.post('/', (req, res, next) => {
+  Chat.create(req.body, (err, chat) => {
+    if (err) return next(err);
+    res.json(chat);
+  });
+});
+
+// DELETE CHAT
+router.delete('/:id', (req, res, next) => {
+  Chat.findByIdAndRemove(req.params.id, req.body, (err, chat) => {
+    if (err) return next(err);
+    res.json(chat);
+  });
 });
 
 module.exports = router;
